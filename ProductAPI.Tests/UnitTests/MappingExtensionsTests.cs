7b808c84e15f6441e83642a005d7a0e315cbf7b7
Filip Číž <cizfilip@live.com>
2024-09-26 00:05:01 +0200

    Added unit tests for mapping

---
+++ ProductAPI.Tests/UnitTests/MappingExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using ProductAPI.Database;
+using ProductAPI.Products;
+
+namespace ProductAPI.Tests.UnitTests;
+
+public class MappingExtensionsTests
+{
+    [Fact]
+    public void ToEntity_FromDto_MapsCorrectly()
+    {
+        // Arrange
+        var dto = new ProductDto
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+            Price = 10
+        };
+
+        // Act
+        var entity = dto.ToEntity();
+
+        // Assert
+        Assert.Equal(dto.Id, entity.Id);
+        Assert.Equal(dto.Name, entity.Name);
+        Assert.Equal(dto.Price, entity.Price);
+    }
+
+    [Fact]
+    public void ToDto_FromEntity_MapsCorrectly()
+    {
+        // Arrange
+        var entity = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+            Price = 10
+        };
+
+        // Act
+        var dto = entity.ToDto();
+
+        // Assert
+        Assert.Equal(entity.Id, dto.Id);
+        Assert.Equal(entity.Name, dto.Name);
+        Assert.Equal(entity.Price, dto.Price);
+    }
+}
